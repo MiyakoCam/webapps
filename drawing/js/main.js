650187ccc6ee4e19b7ff248fd64f1607b1ba912a
こたろー <kotarokotaro123456@gmail.com>
2025-03-09 21:23:34 +0900

    Add files via upload

--- drawing/js/main.js
+++ drawing/js/main.js
@@ -1,10 +1,12 @@
-window.addEventListener('load', () => {
+
   const canvas = document.querySelector('#draw-area');
   const context = canvas.getContext('2d');
 
   // 現在のマウスの位置を中心に、現在選択している線の太さを「○」で表現するために使用するcanvas
   const canvasForWidthIndicator = document.querySelector('#line-width-indicator');
+  const canvasForEraserIndicator = document.querySelector('#line-eraser-indicator');
   const contextForWidthIndicator = canvasForWidthIndicator.getContext('2d');
+  const contextForEraserIndicator = canvasForEraserIndicator.getContext('2d');
 
   const lastPosition = { x: null, y: null };
   let isDrag = false;
@@ -13,6 +15,11 @@ window.addEventListener('load', () => {
   // 現在の線の太さを記憶する変数
   // <input id="range-selector" type="range"> の値と連動する
   var currentLineWidth = 1;
+
+  var isEraser = false;
+  var autoPenMode = true;
+  var beforeColor = '#000000';
+  var beforeLineWidth = 5;
 
   function draw(x, y) {
     if(!isDrag) {
@@ -39,10 +46,10 @@ window.addEventListener('load', () => {
   function showLineWidthIndicator(x, y) {
     contextForWidthIndicator.lineCap = 'round';
     contextForWidthIndicator.lineJoin = 'round';
-    contextForWidthIndicator.strokeStyle = currentColor;
+    contextForWidthIndicator.strokeStyle = isEraser ? '#000000' : currentColor;
 
     // 「○」の線の太さは細くて良いので1で固定
-    contextForWidthIndicator.lineWidth = 5;
+    contextForWidthIndicator.lineWidth = isEraser ? 1 : 4;
 
     // 過去に描画「○」を削除する。過去の「○」を削除しなかった場合は
     // 過去の「○」が残り続けてします。(以下の画像URLを参照)
@@ -54,13 +61,44 @@ window.addEventListener('load', () => {
     // x, y座標を中心とした円(「○」)を描画する。
     // 第3引数の「currentLineWidth / 2」で、実際に描画する線の太さと同じ大きさになる。
     // ドキュメント: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
-    contextForWidthIndicator.arc(x, y, currentLineWidth , 0, 2 * Math.PI);
-
+    if (isEraser) {
+      contextForWidthIndicator.arc(x, y, currentLineWidth / 2 , 0, 2 * Math.PI);
+    } else {
+      contextForWidthIndicator.arc(x, y, currentLineWidth , 0, 2 * Math.PI);
+    }
     contextForWidthIndicator.stroke();
+
+    
   }
 
   function clear() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (document.getElementById('clear-check').checked) {
+      $.confirm({
+        useBootstrap: false,
+        title: '<span class="material-symbols-outlined" style="vertical-align: -27%;font-size: 36px;color: #ff3333;">warning</span> 全て消去しますか？',
+        content: '描画した内容を全て消去します。<br />よろしいですか？',
+        type: 'red',
+        theme: 'material',
+        closeIcon: true,
+        animationSpeed: 400,
+        animationBounce: 1.3,
+        buttons: {
+            キャンセル: {
+              text: 'キャンセル',
+              keys: ['enter', 'shift']
+            },
+            消去: {
+                text: '<span class="material-symbols-outlined" style="vertical-align: -27%;">backspace</span> 全て消去',
+                btnClass: 'btn-red',
+                action: function(){
+                  context.clearRect(0, 0, canvas.width, canvas.height);
+                }
+            }
+        }
+      });
+    } else {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    }
   }
 
   function dragStart(event) {
@@ -70,41 +108,64 @@ window.addEventListener('load', () => {
   }
 
   function dragEnd(event) {
+    
     context.closePath();
     isDrag = false;
     lastPosition.x = null;
     lastPosition.y = null;
+    if (isEraser == true) {
+      isEraser = false;
+      if (autoPenMode) {
+        document.querySelector('#draw-button').click();
+      } else {
+        isEraser = true;
+      }
+    }
   }
 
-  function initEventHandler() {
+  //function initEventHandler() {
     const clearButton = document.querySelector('#clear-button');
+    const drawButton = document.querySelector('#draw-button');
     const eraserButton = document.querySelector('#eraser-button');
     clearButton.addEventListener('click', clear);
+    drawButton.addEventListener('click', () => {
+      currentColor = beforeColor;
+      currentLineWidth = beforeLineWidth;
+      isEraser = false;
+      drawButton.classList.add('on');
+      eraserButton.classList.remove('on');
+    });
     eraserButton.addEventListener('click', () => {
+      beforeColor = currentColor;
+      beforeLineWidth = currentLineWidth;
       currentColor = '#00ff00';
       currentLineWidth = (document.querySelector('#range-selector').value)*4;
+      isEraser = true;
+      eraserButton.classList.add('on');
+      drawButton.classList.remove('on');
     });
     const layeredCanvasArea = document.querySelector('#layerd-canvas-area');
 
     layeredCanvasArea.addEventListener('mousedown', dragStart);
     layeredCanvasArea.addEventListener('mouseup', dragEnd);
-    layeredCanvasArea.addEventListener('mouseout', dragEnd);
+    layeredCanvasArea.addEventListener('mouseleave', dragEnd);
     layeredCanvasArea.addEventListener('mousemove', event => {
       draw(event.layerX, event.layerY);
-
       showLineWidthIndicator(event.layerX, event.layerY);
     });
-  }
+  //}
 
-  function initColorPalette() {
     const joe = colorjoe.rgb('color-palette', currentColor);
     joe.on('done', color => {
-      currentColor = color.hex();
-      currentLineWidth = document.querySelector('#range-selector').value;
+      if (isEraser == true) {
+        beforeColor = color.hex();
+        beforeLineWidth = document.querySelector('#range-selector').value;
+      } else {
+        currentColor = color.hex();
+        currentLineWidth = document.querySelector('#range-selector').value;
+      }
     });
-  }
 
-  function initConfigOfLineWidth() {
     const textForCurrentSize = document.querySelector('#line-width');
     const rangeSelector = document.querySelector('#range-selector');
 
@@ -117,10 +178,9 @@ window.addEventListener('load', () => {
 
       textForCurrentSize.innerText = width;
     });
-  }
 
-  initEventHandler();
-  initColorPalette();
 
-  initConfigOfLineWidth();
-});
+
+  document.getElementById('eraser-check').addEventListener('change', (event) => {
+    autoPenMode = event.target.checked;
+  });
